b15a82100e6dfcc065b8a40b92c128b47c4b90cf
Tommy Back <tommy.jh.back@gmail.com>
2013-05-06 22:14:54 +0200

    Site updated at 2013-05-06 20:14:54 UTC

--- mtgox/javascript/socketio.js
+++ mtgox/javascript/socketio.js
@@ -1,16 +1,38 @@
 var conn = io.connect(uri);
 
 conn.on('connect', function() {
-  var e = document.getElementById('status');
-  e.innerHTML = e.innerHTML + '<div class="notice">Connected to ' + uri + '</div>';
+  logInfo('Connected to ' + uri);
 });
 
-conn.on('message', function(data) {
-    var e = document.getElementById('log');
-    e.innerHTML = e.innerHTML + '<div class="message">' + JSON.stringify(data) + '</div>';
+conn.on('disconnect', function() {
+  logInfo('Disconnected from ' + uri);
 });
 
-conn.on('error', function(data) {
-  var e = document.getElementById('status');
-  e.innerHTML = e.innerHTML + '<div class="error">ERROR: ' + data + '</div>';
+conn.on('message', function(evt) {
+  logMessage(evt);
 });
+
+conn.on('error', function(evt) {
+  logError(evt.data);
+});
+
+function logInfo(message)
+{
+  var pre = document.createElement('div');
+  pre.innerHTML = '<span style="color: blue;"><strong>Info:</strong> ' + message + '</span';
+  document.getElementById('status').appendChild(pre);
+}
+
+function logError(message)
+{
+  var pre = document.createElement('div');
+  pre.innerHTML = '<span style="color: red;"><strong>Error:</strong> ' + message + '</span>';
+  document.getElementById('status').appendChild(pre);
+}
+
+function logMessage(message)
+{
+  var pre = document.createElement('div');
+  pre.innerHTML = '<div class="message">' + JSON.stringify(message) + '</div>';
+  document.getElementById('log').appendChild(pre);
+}
