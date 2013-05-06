71b7c53e4fd91e626b8db18ac8de250c3aa48efa
Tommy Back <tommy.jh.back@gmail.com>
2013-05-06 22:09:06 +0200

    Site updated at 2013-05-06 20:09:06 UTC

--- mtgox/javascript/socketio.js
+++ mtgox/javascript/socketio.js
@@ -1,12 +1,15 @@
 var conn = io.connect(uri);
+
 conn.on('connect', function() {
   var e = document.getElementById('status');
   e.innerHTML = e.innerHTML + '<div class="notice">Connected to ' + uri + '</div>';
 });
+
 conn.on('message', function(data) {
     var e = document.getElementById('log');
     e.innerHTML = e.innerHTML + '<div class="message">' + JSON.stringify(data) + '</div>';
 });
+
 conn.on('error', function(data) {
   var e = document.getElementById('status');
   e.innerHTML = e.innerHTML + '<div class="error">ERROR: ' + data + '</div>';
